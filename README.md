da64a6aa59d4db92fc8d7b2e72378814744d2450
TatriX <tatrics@gmail.com>
2016-10-19 02:34:18 +0300

    Top down view

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# install
+# Installation
 Don't forget to run `git submodule update --init --recursive` or simply put `phaser.min.js` into `phaser/build`
--- src/state.js
+++ src/state.js
@@ -1,98 +1,90 @@
 /* global Phaser */
 
 class State extends Phaser.State {
+    init() {
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+    }
+
     preload() {
-        this.load.image("logo", "assets/logo.png");
-        this.load.image("ground", "assets/ground.png");
         this.load.image("barrel", "assets/barrel.png");
         this.load.image("chest", "assets/chest.png");
         this.load.spritesheet("dude", "assets/dude.png", 64, 82);
+
+        this.load.tilemap("map", "assets/maps/main.json", null, Phaser.Tilemap.TILED_JSON);
+        this.load.image("tiles", "assets/tiles.png");
     }
 
     create() {
-        this.score = 0;
-        this.scoreText = this.add.text(16, 16, "Score: 0", { fontSize: "32px", fill: "#fff" });
-
-        this.logo = this.add.sprite(this.world.centerX, this.world.centerY, "logo");
-        this.logo.anchor.setTo(0.5, 0.5);
-        this.logo.alpha = 0.25;
-        this.logo.Dalpha = 0.001;
-
-        this.physics.startSystem(Phaser.Physics.ARCADE);
+        this.map = this.add.tilemap("map");
+        this.map.addTilesetImage("tiles", "tiles");
+        this.ground = this.map.createLayer("ground");
+        this.walls = this.map.createLayer("walls");
+        this.map.setCollision([4,5,6], true, this.walls);
 
         this.stuff = this.add.group();
         this.stuff.enableBody = true;
 
         for (var i = 0; i < 12; i++) {
-            const shit = this.stuff.create(i * 70, 0, (Math.random() > 0.5) ? "barrel" : "chest");
-            shit.body.gravity.y = 300;
-            shit.body.bounce.y = 0.7 + Math.random() * 0.2;
+            const shit = this.stuff.create(
+                Math.floor(Math.random() * 16) * 64,
+                Math.floor(Math.random() * 16) * 64,
+                (Math.random() > 0.5) ? "barrel" : "chest"
+            );
         }
-
-        this.platforms = this.add.group();
-        this.platforms.enableBody = true;
-
-        const ground = this.platforms.create(0, this.world.height - 64, "ground");
-        ground.scale.setTo(16, 1);
-        ground.body.immovable = true;
-
-        let ledge = this.platforms.create(400, 400, "ground");
-        ledge.body.immovable = true;
-
-        ledge = this.platforms.create(-150, 250, "ground");
-        ledge.body.immovable = true;
-
 
         const player = this.add.sprite(this.world.centerX, this.world.centerY, "dude");
         this.player = player;
         player.anchor.setTo(0.5);
 
         this.physics.arcade.enable(player);
-
-        player.body.bounce.y = 0.2;
-        player.body.gravity.y = 400;
         player.body.collideWorldBounds = true;
+        player.body.setSize(64, 64, 0, 82 - 64);
 
         player.animations.add("left", [0, 1, 2, 3], 10, true);
+        player.animations.add("up", [0, 1, 2, 3], 10, true);
         player.animations.add("right", [0, 1, 2, 3], 10, true);
+        player.animations.add("down", [0, 1, 2, 3], 10, true);
+
+        this.score = 0;
+        this.scoreText = this.add.text(16, 16, "Score: 0", { fontSize: "32px", fill: "#fff" });
     }
 
     update() {
-        if (this.logo.alpha > 0.4 || this.logo.alpha < 0.2) {
-            this.logo.Dalpha = - this.logo.Dalpha;
-        }
-        this.logo.alpha += this.logo.Dalpha;
-
         const player = this.player;
 
-        this.physics.arcade.collide(this.stuff, this.platforms);
+        this.physics.arcade.collide(player, this.walls);
         this.physics.arcade.overlap(player, this.stuff, this.collectStuff.bind(this), null, this);
 
-        const hitPlatform = this.physics.arcade.collide(this.player, this.platforms);
 
         const cursors = this.input.keyboard.createCursorKeys();
-        player.body.velocity.x = 0;
 
-        const speed = 150;
+        player.body.velocity.setTo(0, 0);
+
+        const speed = 250;
         if (cursors.left.isDown) {
             player.body.velocity.x = -speed;
             player.animations.play("left");
         } else if (cursors.right.isDown) {
             player.body.velocity.x = speed;
             player.animations.play("right");
-        } else {
-            player.animations.stop();
-            player.frame = 1;
+        }
+        if (cursors.up.isDown) {
+            player.body.velocity.y = -speed;
+            player.animations.play("up");
+        } else if (cursors.down.isDown) {
+            player.body.velocity.y = speed;
+            player.animations.play("down");
         }
 
-        if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
-            player.body.velocity.y = -350;
+        if (player.body.velocity.x == 0 && player.body.velocity.y == 0){
+            player.animations.stop();
+            player.frame = 1;
         }
     }
 
     collectStuff(player, stuff) {
         stuff.kill();
         this.score += 10;
-        this.scoreText = `Score: ${this.score}`;
+        this.scoreText.text = `Score: ${this.score}`;
     }
 }
